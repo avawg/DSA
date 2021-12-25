633a08c4d4a081b905dec1b364c5873ebc1089db
wg <1182834046@qq.com>
2021-12-25 20:40:32 +0800

    哈夫曼树

--- DataStructures/tree/huffman_tree.py
+++ DataStructures/tree/huffman_tree.py
@@ -1,21 +1,31 @@
 import queue  # 采用堆存储
 
-def huffmanCoding(weights):
+def huffmanCoding(nums):
     que = queue.PriorityQueue()
-    for weight in weights:
-        que.put(weight)  # 建最小堆
-    weight_sum = 0
+    n = len(nums)
+    tree = [0] * (2 * n - 1)
+    parents = [-1 for _ in range(2 * n - 1)]
+    for i in range(n):
+        que.put((nums[i], i))
+        tree[i] = nums[i]
+
+    s = 0
     # 每次从堆中弹出两个最小值，求和后，压入堆中。时间复杂度nlog(n)
-    for _ in range(len(weights) - 1):
-        min_weight1 = que.get()
-        min_weight2 = que.get()
-        weight_sum += min_weight1 + min_weight2
-        que.put(min_weight1 + min_weight2)
-    return weight_sum
+    i = n
+    while i < 2 * n - 1:
+        val1, id1 = que.get()
+        val2, id2 = que.get()
+        tree[i] = val1 + val2
+        parents[id1] = parents[id2] = i
+        que.put((tree[i], i))
+        s += tree[i]
+        i += 1
+    print(tree)
+    print(parents)
+    return s
 
 
 if __name__ == "__main__":
-    weights = [3, 4, 8, 98, 100]
-    min_weight_sum = huffmanCoding(weights)
-    print(min_weight_sum)
-    
+    nums = [3, 4, 8, 98, 100]
+    s = huffmanCoding(nums)
+    print(s)
