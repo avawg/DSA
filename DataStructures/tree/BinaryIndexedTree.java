be625b62afcf93b6fc7ea4db6d83c06aebd75dee
wg <1182834046@qq.com>
2022-04-25 18:38:28 +0800

    树状数组

--- DataStructures/tree/BinaryIndexedTree.java
+++ DataStructures/tree/BinaryIndexedTree.java
@@ -1,41 +1,44 @@
 class BinaryIndexedTree {
 
     // 二叉索引树(binary indexed tree)即树状数组实现，单点修改和区间和查询时间复杂度都为O(logn)
-    // tree[i] 以下标i为终点的lowBit(i)项的和
-    private int[] tree;
+    // tree[i] 以下标i为终点的前lowBit(i)项的和
+    int[] tree;
+    int[] nums;
+    int n;
 
     public BinaryIndexedTree(int[] nums) {
-        tree = new int[nums.length + 1];
-        for (int i = 0; i < nums.length; ++i) {
-            update(i, nums[i]);
+        this.nums = nums;
+        n = nums.length;
+        tree = new int[n + 1];
+        for (int i = 0; i < n; ++i) {
+            add(i + 1, nums[i]);
         }
-    }
-
-    // 单点修改 x增加值，时间复杂度O(logn)
-    public void update(int i, int x) {
-        ++i;
-        while (i < tree.length) {
-            tree[i] += x;
-            i += lowBit(i); // 转到父节点
-        }
-    }
-
-    public int sumRange(int l, int r) {
-        return query(r) - query(l - 1);
-    }
-
-    // 求[0, n]前n项和，时间复杂度O(logn)
-    private int query(int n) {
-        int res = 0;
-        // i -= lowBit(i) 到左侧的父节点
-        for (int i = n + 1; i > 0; i -= lowBit(i)) {
-            res += tree[i];
-        }
-        return res;
     }
 
     private int lowBit(int x) {
         return x & (-x);
+    }
+
+     // 求前x项和，时间复杂度O(logn)
+    private int query(int x) {
+        int ans = 0;
+        // i -= lowBit(i) 到左侧的父节点
+        for (int i = x; i > 0; i -= lowBit(i)) ans += tree[i];
+        return ans;
+    }
+
+    // 单点修改 增加值u，时间复杂度O(logn)
+    private void add(int x, int u) {
+        for (int i = x; i <= n; i += lowBit(i)) tree[i] += u;
+    }
+
+    public void update(int i, int val) {
+        add(i + 1, val - nums[i]);
+        nums[i] = val;
+    }
+
+    public int sumRange(int l, int r) {
+        return query(r + 1) - query(l);
     }
 
     public static void main(String[] args) {
