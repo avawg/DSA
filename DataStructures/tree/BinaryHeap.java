bbdf98560988a516fdff56d2f2bb8fc3a9974539
wg <1182834046@qq.com>
2022-04-28 19:23:49 +0800

    heap

--- DataStructures/tree/BinaryHeap.java
+++ DataStructures/tree/BinaryHeap.java
@@ -1,24 +1,25 @@
 import java.util.HashMap;
 import java.util.Map;
 
-// 支持decrease-key的最小堆, 这里堆中元素是数字 也是map中key, 插入更新后的值不能重复
-// 在图应用中, key可以是节点id 没有限制
+/**
+ *  模拟实现二叉堆 小顶堆
+ */
 public class BinaryHeap {
 
     private int[] nums;
     private int size;
-    private Map<Integer, Integer> entry = new HashMap<>(); // 记录节点在堆中位置
 
     public BinaryHeap(int n) {
         nums = new int[n];
         size = 0;
     }
 
-    // 插入元素，时间复杂度O(logn)
-    public void add(int val) {
+    /**
+     * 插入元素，时间复杂度O(logn)
+     */
+    public void offer(int val) {
         nums[size] = val;
-        entry.put(val, size++);
-        for (int i = size - 1; i > 0; ) {
+        for (int i = size; i > 0; ) {
             if (nums[i] < nums[(i - 1) / 2]) {
                 swap(i, (i - 1) / 2);
                 i = (i - 1) / 2;
@@ -26,14 +27,15 @@ public class BinaryHeap {
                 break;
             }
         }
+        size++;
     }
 
-    // 删除最小元素，时间复杂度O(logn)
-    public int remove() {
-        int res = nums[0];
-        entry.remove(nums[0]);
-        // 将数组尾元素移到首部
-        entry.put(nums[0] = nums[--size], 0);
+    /**
+     * 删除最小元素，时间复杂度O(logn)
+     */
+    public int poll() {
+        int ans = nums[0];
+        nums[0] = nums[--size];
         // 更新堆
         for (int i = 0; 2 * i + 1 < size; ) {
             int j = 2 * i + 1;
@@ -47,43 +49,25 @@ public class BinaryHeap {
                 i = j;
             }
         }
-        return res;
-    }
-
-    // 降低堆中某个元素的值，时间复杂度O(logn)
-    public void decrease(int val, int newVal) {
-        int i = entry.get(val);
-        nums[i] = newVal;
-        while (i > 0) {
-            if (nums[i] < nums[(i - 1) / 2]) {
-                swap(i, (i - 1) / 2);
-                i = (i - 1) / 2;
-            } else {
-                break;
-            }
-        }
+        return ans;
     }
 
     public boolean isEmpty() {
         return size == 0;
     }
 
-    // 交换min-heap中值，并更新key映射
     private void swap(int i, int j) {
-        int temp = nums[i];
+        int t = nums[i];
         nums[i] = nums[j];
-        nums[j] = temp;
-        entry.put(nums[i], i);
-        entry.put(nums[j], j);
+        nums[j] = t;
     }
 
     public static void main(String[] args) {
         BinaryHeap heap = new BinaryHeap(10);
         for (int i = 0; i < 10; i++) {
-            heap.add(2 * i + 1);
+            heap.offer(2 * i + 1);
         }
-        System.out.println(heap.remove());
-        heap.decrease(11, -10);
-        System.out.println(heap.remove());
+        System.out.println(heap.poll());
+        System.out.println(heap.poll());
     }
 }
