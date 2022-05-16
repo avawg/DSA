57a1b97c4b41fa51d3155dc50a5afe559366a3ea
wg <1182834046@qq.com>
2022-05-16 12:02:13 +0800

    捡西瓜

--- Search dp/捡西瓜.cpp
+++ Search dp/捡西瓜.cpp
@@ -1,11 +1,12 @@
 /**
- * n个西瓜排成一列，每个西瓜有两个属性（weight, skip);weight表示西瓜的重量，skip表示捡完；
- * 该西瓜后，就必须跳过接下来的skip个西瓜[i + 1, i + skip]，即不能捡起它们。
+ * n个西瓜排成一列，每个西瓜有两个属性（weight, skip);weight表示西瓜的重量，skip表示捡完该西瓜后，
+ * 就必须跳过接下来的skip个西瓜[i + 1, i + skip]，即不能捡起它们。
  * 经过每个西瓜的必须选择捡或不捡，不能回头；输出能捡起西瓜的最大质量
  */
 #include <iostream>
 using namespace std;
 
+int ans = 0;
 const int MAX = 100001;
 int weight[MAX], skip[MAX];
 int dp[MAX];
@@ -20,10 +21,10 @@ int main()
         cin >> weight[i] >> skip[i];
     }
     for (int i = 0; i < n; i++) {
-        dp[i] = 0;
+        dp[i] = -1;
     }
     dfs(0, n, 0);
-    cout << dp[n - 1];
+    cout << ans;
     return 0;
 }
 
@@ -31,17 +32,16 @@ int main()
 // dp[i]表示前0-i个西瓜能获得的最大质量
 void dfs(int i, int n, int score) {
 	if (i >= n) {
+		ans = score > ans ? score : ans;
 		return ;
 	}
-	// 捡第i个西瓜
-	int end = min(n - 1, i + skip[i]);
-	if (score + weight[i] >= dp[end]) {
-		dp[end] = score + weight[i];
-		dfs(end + 1, n, score + weight[i]);
-	}
-	// 不捡第i个西瓜
-	if (score >= dp[i]) {
+	if (score <= dp[i]) {
+		return ;
+	} else {
 		dp[i] = score;
-		dfs(i + 1, n, score);
 	}
+	// 捡第i个西瓜
+	dfs(i + skip[i] + 1, n, score + weight[i]);
+	// 不捡第i个西瓜
+	dfs(i + 1, n, score);
 }
