7038767b9f3a723ac3b2d62e09e82a007d4a8b74
王钢 <1182834046@qq.com>
2022-07-27 22:58:01 +0800

    quick sort

---
+++ template/quick_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void qsort(int nums[], int l, int r) {
+    if (l >= r) return ;
+    // 一次快排, 将 <= pivot元素移动到左侧，>= pivot元素移动到右侧
+    int mid = (l + r) / 2;
+    swap(nums[l], nums[mid]);
+    int pivot = nums[l];
+    int j = l + 1;
+    for (int i = l + 1; i <= r; i++) {
+        if (nums[i] <= pivot) {
+            swap(nums[j], nums[i]); j++;
+        }
+    }
+    j--;
+    swap(nums[l], nums[j]);
+    qsort(nums, l, j - 1);
+    qsort(nums, j + 1, r);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int nums[n];
+    for (int i = 0; i < n; i++) scanf("%d", &nums[i]);
+    qsort(nums, 0, n - 1);
+    for (int i = 0; i < n; i++) printf("%d ", nums[i]);
+    return 0;
+}
