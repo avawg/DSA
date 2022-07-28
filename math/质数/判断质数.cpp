796bb1fe9241f61cf2902b29939f1ecfdc51bf88
王钢 <1182834046@qq.com>
2022-07-29 01:41:15 +0800

    判断质数

--- math/质数/判断质数.cpp
+++ math/质数/判断质数.cpp
@@ -9,7 +9,7 @@ int main() {
         if (x == 1) puts("No");
         else {
             bool flag = true;
-            // 两个因子必定成对出现
+            // 两个因子必定成对出现, O(sqrt(n))
             for (int i = 2; i <= x / i; i++) {
                 if (x % i == 0) {
                     flag = false; break;
